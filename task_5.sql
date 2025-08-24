de51d4a9fdf3d422c0f3b789fc6b2fe4953f554b
brianstatesthat <mbspeelman@gmail.com>
2025-08-24 11:00:03 +0200

    task 5 added adding customer name added

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,6 @@
 -- task_5.sql
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+DELETE FROM customer WHERE customer_id = 1;
+
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
