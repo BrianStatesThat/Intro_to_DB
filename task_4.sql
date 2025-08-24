c3e2612bff65145e6661f83b7030daa2a57df281
brianstatesthat <mbspeelman@gmail.com>
2025-08-24 10:53:16 +0200

    task 4 added

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,8 @@
--- task_4.sql
 SELECT 
     COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
+    COLUMN_TYPE 
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books' 
-ORDER BY 
-    ORDINAL_POSITION;
+    AND TABLE_NAME = 'Books';
